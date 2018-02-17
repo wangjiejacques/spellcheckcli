6eaa06dad4f1defe07c6dc87e2804b5fc66b5c59
WANG Jie <jacques09125715@gmail.com>
2018-02-17 15:28:22 +0100

    Initial Commit

---
+++ spellcheckcli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  spellcheckcli
+//
+//  Created by WANG Jie on 17/02/2018.
+//  Copyright © 2018 jwang123. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
